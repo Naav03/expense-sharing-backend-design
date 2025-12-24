389c0fe0f04371be4c6859ef7224af00a6584a49
Naav03 <148970944+Naav03@users.noreply.github.com>
2025-12-24 11:47:18 +0530

    Add files via upload

---
+++ ExpenseService.java
@@ -0,0 +1,12 @@
+public class ExpenseService {
+
+    public void addExpense() {
+        // Validate split
+        // Update balances
+        // Simplify balances
+    }
+
+    public void settleExpense() {
+        // Settlement logic
+    }
+}
